e754a0670b1009b30ba767f08f79aab595a9ec6f
angoloisaac83 <angoloisaac83@gmail.com>
2024-08-13 14:54:59 -0700

    new

--- src/components/landingPage/AppLayout.jsx
+++ src/components/landingPage/AppLayout.jsx
@@ -12,14 +12,20 @@ const menu = () => {
     return(
         <>
         <main className='flex flex-col gap-[10px] w-full h-fit overflow-y-auto items-cenetr justify-center'>
-            <nav className="flex w-full shadow-xl h-fit py-[7px] items-center justify-between px-[10px]">
-                <span className='w-fit h-fit flex text-3xl items-center justify-center'>
-                    <i className='font-semibold'>Payyed</i>
-                    <button onClick={menu}><IoMenuOutline /></button>
+            <nav className="flex w-full desktop:px-[50px] shadow-xl h-fit py-[7px] items-center justify-between px-[10px]">
+                <span className='w-fit h-fit desktop:gap-[30px] flex items-center justify-center'>
+                    <i className='font-semibold text-3xl'>Payyed</i>
+                <ul className='w-fit hidden transition-all desktop:flex p-[10px] font-medium z-[100] bg-white text-md gap-3 left-0'>
+                                <li>SEND</li>
+                                <li>RECIEVED</li>
+                                <li>ABOUT US</li>
+                                <li>FEES</li>
+                            </ul>
+                    <button onClick={menu} className='desktop:hidden flex'><IoMenuOutline /></button>
                 </span>
                 <span className='flex text-xl font-semibold gap-[5px] items-center justify-center w-fit h-fit'>
                     <button>Login</button>
-                    <button className='bg-green-500 text-white p-[10px] rounded-md'>Sign Up</button>
+                    <button className='bg-green-500 text-white py-[6px] p-[10px] rounded-md'>Sign Up</button>
                 </span>
                 {
                     isOpen ? (
@@ -39,19 +45,20 @@ const menu = () => {
             <div className='w-full h-fit flex items-center justify-center text-xl'>
                 {children}
             </div>
-            <footer className='w-full h-fit text-md font-semibold text-center flex flex-col items-center justify-center pt-[20px] bg-slate-50'>
-                <ul className='flex gap-6 w-fit'>
+            <footer className='w-full h-fit desktop:flex-row desktop:justify-between desktop:px-[5%] desktop:py-[50px] text-md font-semibold text-center flex flex-col items-center justify-center pt-[20px] bg-slate-50'>
+            <span className='desktop:flex desktop:gap-[7px] desktop:flex-col'><ul className='flex gap-6 w-fit'>
                     <li>About Us</li>
                     <li>Support</li>
                     <li>Help</li>
                     <li>Carrers</li>
                 </ul>
+                <hr />
                 <span className='flex py-[15px] gap-8'>
                     <FaFacebookF />
                     <FaXTwitter />
                     <FaYoutube />
-                </span>
-                <span>
+                </span></span>
+                <span className='desktop:flex desktop:flex-col'>
                     <p className='text-md'>Copyright @ 2022 Payyed. All Rights Reserved.</p>
                     <ul className='py-[15px] text-md flex items-center justify-center gap-6'>
                         <li>Security</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        desktop: {'min': '500px'}
+      },
+    },
   },
   plugins: [],
 }
